959c481a2140e89ce98fc595a2e1735f176f72c4
Surya <surya.saravanakumar@enparadigm.com>
2021-05-03 21:55:34 +0530

    Added call back for checkbox

--- todo_flutter/lib/Widgets/TaskListTile.dart
+++ todo_flutter/lib/Widgets/TaskListTile.dart
@@ -1,36 +1,47 @@
 import 'package:flutter/material.dart';
 
-class TaskTileCell extends StatelessWidget {
-  const TaskTileCell({
-    Key key,
-  }) : super(key: key);
+class TaskTileCell extends StatefulWidget {
+  @override
+  _TaskTileCellState createState() => _TaskTileCellState();
+}
+
+class _TaskTileCellState extends State<TaskTileCell> {
+  var isChecked = false;
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text('This is list view'),
-      trailing: TaskCheckBox(),
+      title: Text(
+          'This is list view',
+        style: TextStyle(
+          decoration: isChecked ? TextDecoration.lineThrough : null
+        ),
+      ),
+      trailing: TaskCheckBox(
+          checkBoxState: isChecked,
+          //This is call back function (like our baby closure)
+          checkBoxStateChanged: (bool newValue){
+            setState(() {
+              isChecked =  newValue;
+            });
+          }
+      ),
     );
   }
 }
 
 
-class TaskCheckBox extends StatefulWidget {
-  @override
-  _TaskCheckBoxState createState() => _TaskCheckBoxState();
-}
+class TaskCheckBox extends StatelessWidget {
+  final bool checkBoxState;
+  final Function checkBoxStateChanged;
 
-class _TaskCheckBoxState extends State<TaskCheckBox> {
-
-  var isChecked = false;
+ TaskCheckBox({this.checkBoxState, this.checkBoxStateChanged});
 
   @override
   Widget build(BuildContext context) {
     return  Checkbox(
-      value: isChecked,
-      onChanged: (newValue){
-        isChecked = newValue;
-      },
+      value: checkBoxState,
+      onChanged: checkBoxStateChanged
     );
   }
 }
